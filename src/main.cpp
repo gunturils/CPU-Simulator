7069472d2767cb1b75321f64af1e1b6d66f01074
gunturils <gunturils20@github.com>
2026-05-28 18:52:06 -0500

    Creation of load, add, and print Registers functions in order to simulate the load and add operations and a debugging tool

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 
 struct RegisterFile {
-	int registers[8]; // represents having 8 general purpose registers
+	int registers[8] = {0, 0, 0, 0, 0, 0, 0, 0}; // represents having 8 general purpose registers
 };
 
 struct Memory {
 	unsigned char data[256]; // represents 256 bytes of memory
 	// each 'memory' location (0-255) is addressable by index
 };
+
+void load(RegisterFile& rf, Memory& mem, int reg, int add) {
+	rf.registers[reg] = mem.data[add];
+}
+
+void add(RegisterFile& rf, int dest, int src1, int src2) {
+	rf.registers[dest] = rf.registers[src1] + rf.registers[src2];
+}
+
+void printReg(RegisterFile& rf) {
+	for (int i = 0; i < 8; i++) {
+		std::cout << "R" << i << ": " << rf.registers[i] << std::endl;
+	}
+}
 
 int main() {
 	RegisterFile rf;
@@ -17,15 +31,12 @@ int main() {
 	mem.data[0] = 42;
 	mem.data[1] = 99;
 
-	rf.registers[0] = mem.data[0]; // R0 has the value in memory location 0
-	rf.registers[1] = mem.data[1]; // R1 has the value in memory location 1
-	rf.registers[2] = rf.registers[0] + rf.registers[1];
-
-	std::cout << "Memory[0]: " << (int)mem.data[0]  << std::endl;
-	std::cout << "Memory[1]: " << (int)mem.data[1]  << std::endl;
-	std::cout << "R0: " << rf.registers[0] << std::endl;
-	std::cout << "R1: " << rf.registers[1] << std::endl;
-	std::cout << "R2 = R0 + R1: " << rf.registers[2] << std::endl;
+	load(rf, mem, 0, 0); // loads memory byte 0 into R0
+	load(rf, mem, 1, 1); // loads memory byte 1 into R1
+	add(rf, 2, 0, 1); // loads R0+R1 into R2
+	
+	printReg(rf);
 
 	return 0;
 }
+
