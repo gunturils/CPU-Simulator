49bafb2ce4fbabe605daad7474c284fdc4d5ff36
gunturils <gunturils20@github.com>
2026-05-28 19:19:32 -0500

    creation of enum variables to represent 7 instruction types and print functions to create a debugging tool

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+enum class Opcode {
+	ADD,
+	SUB,
+	LOAD,
+	STORE, 
+	JMP,
+	BEQ,
+	HALT
+};
+
+struct Instruction {
+	Opcode op;
+	int dest;
+	int src1;
+	int src2;
+	int imm;
+};
+
 struct RegisterFile {
 	int registers[8] = {0, 0, 0, 0, 0, 0, 0, 0}; // represents having 8 general purpose registers
 };
@@ -9,33 +27,64 @@ struct Memory {
 	// each 'memory' location (0-255) is addressable by index
 };
 
-void load(RegisterFile& rf, Memory& mem, int reg, int add) {
-	rf.registers[reg] = mem.data[add];
+void printInstruction(Instruction instr) {
+	switch (instr.op) {
+		case Opcode::ADD:
+			std::cout << "ADD R" << instr.dest
+				<< ", R" << instr.src1
+				<< ", R" << instr.src2 << std::endl;
+			break;
+		case Opcode::SUB:
+			std::cout << "SUB R" << instr.dest
+                                << ", R" << instr.src1
+                                << ", R" << instr.src2 << std::endl;
+			break;
+		case Opcode::LOAD:
+			std::cout << "LOAD R" << instr.dest
+				<< ", " << instr.imm << std::endl;
+			break;
+		case Opcode::STORE:
+			std::cout << "STORE R" << instr.src1
+				<< ", " << instr.imm << std::endl;
+			break;
+		case Opcode::JMP:
+			std::cout << "JMP " << instr.imm << std::endl;
+			break;
+		case Opcode::BEQ:
+			std::cout << "BEQ R" << instr.src1
+				<< ", R" << instr.src2
+				<< ", " << instr.imm << std::endl;
+			break;
+		case Opcode::HALT:
+			std::cout << "HALT" << std::endl;
+			break;
+	}
 }
 
-void add(RegisterFile& rf, int dest, int src1, int src2) {
-	rf.registers[dest] = rf.registers[src1] + rf.registers[src2];
-}
-
-void printReg(RegisterFile& rf) {
+void printRegisters(RegisterFile& rf) {
 	for (int i = 0; i < 8; i++) {
 		std::cout << "R" << i << ": " << rf.registers[i] << std::endl;
 	}
 }
 
 int main() {
-	RegisterFile rf;
+	Instruction instr1; //creates instruction variable called instr1
+	instr1.op = Opcode::LOAD;
+	instr1.dest = 0;
+	instr1.imm = 42;
 
-	Memory mem; 
+	Instruction instr2;
+	instr2.op = Opcode::ADD;
+	instr2.dest = 2;
+	instr2.src1 = 0;
+	instr2.src2 = 1;
 
-	mem.data[0] = 42;
-	mem.data[1] = 99;
+	Instruction instr3;
+	instr3.op = Opcode::HALT;
 
-	load(rf, mem, 0, 0); // loads memory byte 0 into R0
-	load(rf, mem, 1, 1); // loads memory byte 1 into R1
-	add(rf, 2, 0, 1); // loads R0+R1 into R2
-	
-	printReg(rf);
+	printInstruction(instr1); // verifies that the instructions are printing correctly
+	printInstruction(instr2);
+	printInstruction(instr3);
 
 	return 0;
 }
